c4ccdecee249ce7dcc25a2bb10a52f670cbbdd17
Cha Yang <chachaiyang3@gmail.com>
2019-09-18 21:08:24 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,7 +14,7 @@ class EmailAddressParser
     @email_addresses.split(",\s")
   end
   
-email_addresses = "john@doe.com, person@somewhere.org"
+email_addresses = email_addresses.parse
 parser = EmailAddressParser.new(email_addresses)
  
 parser.parse
